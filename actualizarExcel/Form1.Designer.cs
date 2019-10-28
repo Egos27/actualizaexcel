cf2d6b908b8027737ab90e69c387d4eaf2265fea
eoggioni <eoggioni@aga-archivos.com.ar>
2019-10-28 13:01:19 -0300

    first commit

---
+++ actualizarExcel/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace actualizarExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(63, 62);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(296, 48);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(63, 125);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 1;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 228);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ actualizarExcel/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace actualizarExcel
+{
+    public partial class Form1 : Form
+    {
+
+        bool sigue = true;
+        string salida;
+        string url;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.webBrowser1.ScriptErrorsSuppressed = true;            
+            this.webBrowser1.DocumentCompleted += WebBrowser1_DocumentCompleted;
+        }
+
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+
+            salida = "";
+            this.sigue = true;
+            string path = "archivo.csv";
+
+            this.openFileDialog1.FileName = "";
+            this.openFileDialog1.Filter = "Comma Separation Values|*.csv";
+            this.openFileDialog1.ShowDialog();
+            if (!File.Exists(this.openFileDialog1.FileName))
+            {
+                MessageBox.Show("error, no existe el archivo");
+                return;
+            }
+
+            path = this.openFileDialog1.FileName;
+
+            var archivo = File.OpenText(path);
+            while (!archivo.EndOfStream)
+            {
+                var linea = archivo.ReadLine();
+                this.url = linea.IndexOf(";") == -1 ? linea.Substring(0) : linea.Substring(0, linea.IndexOf(";") );
+                this.sigue = false;
+                this.webBrowser1.Navigate(url);
+                while (!this.sigue) Application.DoEvents();
+                //"https://www.nubbeo.com.ar/sensor-digital-temperatura-ds18b20-cable-sumergible-nubbeo-549563105xJM"
+            }
+            archivo.Close();    
+            path = "archivo1.csv";
+            File.WriteAllText(path, this.salida);
+            System.Diagnostics.Process.Start(Path.GetFullPath(path));
+
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+            if (e.Url.AbsoluteUri==this.url)
+            {
+                try
+                {
+                    //var a = this.webBrowser1.Document.GetElementById("Price").GetAttribute("Value");
+                    var a = this.buscarValor();
+
+                    this.salida += e.Url.AbsoluteUri + ";" + a + ";\n";
+                }
+                catch
+                {
+                    this.salida += e.Url.AbsoluteUri + ";" + ";\n";
+                }
+                this.sigue = true;
+            }
+        }
+
+           private string buscarValor()
+        {
+            HtmlElementCollection theElementCollection = this.webBrowser1.Document.GetElementsByTagName("span");
+            foreach (HtmlElement curElement in theElementCollection)
+            {
+                if (curElement.GetAttribute("className").ToString() == "money")
+                {
+                    //MessageBox.Show(curElement.GetAttribute("InnerText")); // Do something you want
+                    return curElement.GetAttribute("InnerText").Trim();
+                }
+            }
+            return ""; 
+        }
+                     
+
+    }
+
+ }
+
+
